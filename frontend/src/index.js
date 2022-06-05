a480561e64daff3ca45e40eaa2dc770fb46393d8
pepermz <jose.ram.18@gmail.com>
2022-06-05 16:01:28 -0600

    adding chakraui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* In order to have chakra working we have to wrap app inide chakra provider tags */}
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
