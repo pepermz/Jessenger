6641024a0f206fd641725bb89a37dc00b511fa9a
pepermz <jose.ram.18@gmail.com>
2022-06-05 16:09:50 -0600

    installed dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 
-import { Button } from '@chakra-ui/react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Button colorScheme='blue'>Button</Button>
+ 
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* In order to have chakra working we have to wrap app inide chakra provider tags */}
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      {/* In order to have chakra working we have to wrap app inide chakra provider tags */}
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
