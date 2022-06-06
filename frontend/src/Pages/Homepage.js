ebf5cd3b819b84ef0604aa4067e6b556f7151ea9
pepermz <jose.ram.18@gmail.com>
2022-06-06 08:20:41 -0600

    created components

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,33 @@
 import React from 'react'
-import { Container } from '@chakra-ui/react'
+import { Box, Container, Text, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
+
+
 const Homepage = () => {
   return (
-    <Container>
-
+    <Container maxW='xl' centerContent>
+      {/* adding inline styling */}
+      {/* d = display p = padding bg = background w= width m= margin */}
+      <Box d='flex' justifyContent='center' p={3} bg={'white'} w="100%" m="40px 0 15px 0" borderRadius="lg" borderWidth="1px">
+        <Text fontSize="1xl" textAlign='center'>Jesenger</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs absvariant='enclosed'>
+          <TabList mb='1em'>
+            <Tab width="50%">Log In</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Container>
   )
 }
